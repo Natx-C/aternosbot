3dfaefe7bf6f009dc7ec944a7d512eb0e3656147
乂Nat <83612604+Natx-C@users.noreply.github.com>
2022-01-15 01:36:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var mineflayer = require('mineflayer')
 var bot = mineflayer.createBot({
   host: "natan1199.aternos.me",   
   username: "PireCraftyBot",
-  version: "1.17.1"
+  version: "1.16.0"
 })
 
 bot.on('login', async () => {
